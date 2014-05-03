69434b7a6ddda0e50e3df8ebf5511b94cf1935f3
maggiechang <maggie.bien.chang@sbcglobal.net>
2014-05-03 11:37:15 -0700

    Revert "Joy Button !!"
    This reverts commit 3541b6dfd3633835115c8a0c90eafd3005fcf1cb.

--- src/JoyButton.cpp
+++ src/JoyButton.cpp
@@ -1,33 +1,23 @@
 #include <JoyButton.hpp>
 
 JoyButton::JoyButton()
-	: name()
 {
-	down = false;
-	pressed = false;
-	up = false;
+
 }
 
 JoyButton::JoyButton(ButtonNames name)
-	: name(name)
 {
 	std::cout << "Button #2" << std::endl;
 }
 
 JoyButton::JoyButton(bool wouldBeDown, bool wouldBePressed, bool wouldBeUp, ButtonNames name)
-	: down(wouldBeDown)
-	, pressed(wouldBePressed)
-	, up(wouldBeUp)
-	, name(name)
 {
 	std::cout << "Button #1" << std::endl;
 }
 
 inline bool JoyButton::operator==(const JoyButton& right)
 {
-	if ((down == right.down) && (pressed == right.pressed) && (up == right.up) && (name == right.name))
-		return true;
-	else
-		return false;
 	std::cout << "Operator== JoyButton" << std::endl;
+	//TODO fix
+	return true;
 }
