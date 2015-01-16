7c1d55757be214bcd0ae75e036d5a68bae6a6c98
Emily Tam <18tame94@burlingamehighschool.org>
2015-01-16 15:25:56 -0800

    classes

---
+++ src/ActionMap.hpp
@@ -0,0 +1,6 @@
+#ifndef ACTIONMAP_HPP
+#define ACTIONMAP_HPP
+
+private std::vector<std::pair<>> ButtonMap
+public void register()
+public bool EventOcurredFor();
---
+++ src/action.hpp
@@ -0,0 +1,16 @@
+#include "main.cpp"
+
+int main()
+{
+	public
+	{
+	const std::function callback;
+	}
+
+
+	private
+	{
+
+	}
+}
+
