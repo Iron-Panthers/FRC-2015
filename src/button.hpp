e4115c94eb15a728949c5c1f570661fd484986d3
Isabel Dominik <icd107@live.com>
2015-01-17 13:24:48 -0800

    fixed button

--- src/button.hpp
+++ src/button.hpp
@@ -1,47 +1,21 @@
 #ifndef BUTTON_HPP
 #define BUTTON_HPP
+
+#include <WPILib.h>
+#include <iostream>
+
+class Button
+{
 public:
-bool down
-{
-if
-{
-return true;
-}
-else
-{
-return false;
-}
-}
-public:
-bool pressed
-{
-if
-{
-return true;
-}
-else
-{
-return false;
-}
-}
-public:
-bool up
-{
-if
-{
-return true;
-}
-else
-{
-return false;
-}
-}
-public:
-inline bool operator==(const Button& left, const Button& right)
-public:
-Button(ButtonNames name)
-public:
-Button(bool wouldBeDown, bool wouldBePressed, bool wouldBeUp, ButtonNames name)
+	bool down;
+	bool pressed;
+	bool up;
+	inline bool operator==(const Button& left, const Button& right);
+	Button(ButtonNames name);
+	Button(bool wouldBeDown, bool wouldBePressed, bool wouldBeUp, ButtonNames name);
+
 private:
-ButtonNames name
+	ButtonNames name;
+};
+
 #endif
