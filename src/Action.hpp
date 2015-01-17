52e22bab31f68a1668ec5de28358d00212190593
Isabel Dominik <icd107@live.com>
2015-01-17 13:12:16 -0800

    add back action

---
+++ src/Action.hpp
@@ -0,0 +1,17 @@
+#ifndef ACTION_HPP
+#define ACTION_HPP
+
+#include <functional>
+
+template <typename T>
+class Action
+{
+public:
+	const int cost;
+	Action(std::function<T> func, int cost);
+	void run() const;
+private:
+	const std::function<T> callback;
+};
+
+#endif
