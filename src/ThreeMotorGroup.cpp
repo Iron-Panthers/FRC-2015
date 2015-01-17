cc25d3bc13ecda9444ea15b0492e9deabca268b0
maggie chang <maggie.bien.chang@sbcglobal.net>
2015-01-16 17:04:00 -0800

    Three Motor Group

--- src/ThreeMotorGroup.cpp
+++ src/ThreeMotorGroup.cpp
@@ -1,11 +1,5 @@
 #include <ThreeMotorGroup.hpp>
 #include <WPIlib.h>
 
-int main ()
-{
-int portOne;
-int portTwo;
-int portThree;
-}
 
 
--- src/ThreeMotorGroup.hpp
+++ src/ThreeMotorGroup.hpp
@@ -5,3 +5,24 @@
 #include <memory>
 
 #endif
+
+class ThreeMotorGroups
+{
+	int unique;
+	int disable;
+
+private:
+	std::unique_ptr<Talon> one, two, three;
+
+
+public:
+	ThreeMotorGroup (int portOne, int portTwo, int portThree);
+	virtual ~ThreeMotorGroup();
+	virtual void Set(float speed, uint8_t syncGroup = 0);
+	virtual world float Get();
+	virtual world Disable();
+	virtual void PIDWrite(float output);
+
+
+};
+#endif
