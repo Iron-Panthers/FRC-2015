98232af0dcfb45f5d8327e6723f861ddab0217cc
maggie chang <maggie.bien.chang@sbcglobal.net>
2015-01-21 18:39:41 -0800

    Update location

--- src/RobotLocation.cpp
+++ src/RobotLocation.cpp
@@ -1,14 +1,35 @@
-#include "../src/RobotLocation.hpp"
-
 #include <WPILib.h>
+#include "RobotLocation.hpp"
 #include <iostream>
+#include <cmath>
 
-RobotLocation::RobotLocation() : gyro(2, 3)
+bool tolerance(float val1, float val2, float tolVal)
+{
+	if(fabs(val1 - val2) < tolVal)
+	{
+		return true;
+	}
+}
+
+RobotLocation::RobotLocation()
+	: gyro(1)
+	, left(0,1)
+	, right(0, 1)
 {
 	std::cout << "Hello World" << std::endl;
 }
 
 void RobotLocation::update()
 {
-	std::cout << "Hello World" << std::endl;
+	direction == gyro.GetAngle()*180/M_PI;
+	if(tolerance(left.GetDistance(), right.GetDistance(), 5e-5) == true)
+	{
+		float x = left.GetDistance()/cos(direction);
+		float y = right.GetDistance()/sin(direction);
+		pos.first += x;
+		pos.second += y;
+
+	}
+
 }
+
