e42dc52cd095a99b0bd79bef8241f972aabe30ac
Leon Cheung <leonjcheung@gmail.com>
2015-03-17 22:34:51 -0700

    new unittest area

---
+++ src/JoyTest.cpp
@@ -0,0 +1,28 @@
+#include "JoyTest.hpp"
+#include <iostream>
+
+void JoyTest::button11()  { std::cout << "joystick 1, button 1"  << std::endl; }
+void JoyTest::button12()  { std::cout << "joystick 1, button 2"  << std::endl; }
+void JoyTest::button13()  { std::cout << "joystick 1, button 3"  << std::endl; }
+void JoyTest::button14()  { std::cout << "joystick 1, button 4"  << std::endl; }
+void JoyTest::button15()  { std::cout << "joystick 1, button 5"  << std::endl; }
+void JoyTest::button16()  { std::cout << "joystick 1, button 6"  << std::endl; }
+void JoyTest::button17()  { std::cout << "joystick 1, button 7"  << std::endl; }
+void JoyTest::button18()  { std::cout << "joystick 1, button 8"  << std::endl; }
+void JoyTest::button19()  { std::cout << "joystick 1, button 9"  << std::endl; }
+void JoyTest::button110() { std::cout << "joystick 1, button 10" << std::endl; }
+void JoyTest::button111() { std::cout << "joystick 1, button 11" << std::endl; }
+void JoyTest::button112() { std::cout << "joystick 1, button 12" << std::endl; }
+
+void JoyTest::button21()  { std::cout << "joystick 2, button 1" << std::endl;  }
+void JoyTest::button22()  { std::cout << "joystick 2, button 2" << std::endl;  }
+void JoyTest::button23()  { std::cout << "joystick 2, button 3" << std::endl;  }
+void JoyTest::button24()  { std::cout << "joystick 2, button 4" << std::endl;  }
+void JoyTest::button25()  { std::cout << "joystick 2, button 5" << std::endl;  }
+void JoyTest::button26()  { std::cout << "joystick 2, button 6" << std::endl;  }
+void JoyTest::button27()  { std::cout << "joystick 2, button 7" << std::endl;  }
+void JoyTest::button28()  { std::cout << "joystick 2, button 8" << std::endl;  }
+void JoyTest::button29()  { std::cout << "joystick 2, button 9" << std::endl;  }
+void JoyTest::button210() { std::cout << "joystick 2, button 10" << std::endl; }
+void JoyTest::button211() { std::cout << "joystick 2, button 11" << std::endl; }
+void JoyTest::button212() { std::cout << "joystick 2, button 12" << std::endl; }
---
+++ src/JoyTest.hpp
@@ -0,0 +1,34 @@
+#ifndef JOYTEST_HPP
+#define JOYTEST_HPP
+
+class JoyTest
+{
+public:
+	void button11();
+	void button12();
+	void button13();
+	void button14();
+	void button15();
+	void button16();
+	void button17();
+	void button18();
+	void button19();
+	void button110();
+	void button111();
+	void button112();
+
+	void button21();
+	void button22();
+	void button23();
+	void button24();
+	void button25();
+	void button26();
+	void button27();
+	void button28();
+	void button29();
+	void button210();
+	void button211();
+	void button212();
+};
+
+#endif
